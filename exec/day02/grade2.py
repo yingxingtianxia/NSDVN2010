4511623cb166ac0546742c3819fb10162ed9c972
yingxingtianxia <903511044@qq.com>
2021-06-08 21:52:30 +0800

    2021-06-08 21:52:30

--- exec/day02/grade2.py
+++ exec/day02/grade2.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
-'''
-从终端获取分数，根据分数判断级别
-'''
 score = int(input('请输入分数：'))
-# int()    '100' --> 100
-print(type(score))
 
-if score < 60:
+if score < 0 or score > 100:
+    print('输入不合理')
+elif 0 <= score < 60:
     print('不及格，你该努力了')
 elif 60 <= score < 80:
     print('良好')
-elif 80 <= score <=100:
+elif 80 <= score <= 100:
     print('优秀')
