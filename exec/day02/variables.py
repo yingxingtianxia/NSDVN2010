d1cfd5a31e95f40151c39c7c77ea2880d7f9afe9
yingxingtianxia <903511044@qq.com>
2021-06-08 20:57:13 +0800

    2021-06-08 20:57:12

---
+++ exec/day02/variables.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# 我的GitHub地址
+# https://github.com/yingxingtianxia/NSDVN2010.git
+
+# Python的变量类型
+数字
+>>> a = 10
+>>> b = '10'
+>>> type(a)
+<class 'int'>
+>>> type(b)
+<class 'str'>
+>>>
+
+浮点数
+复数
+布尔值
+字符串
+列表
+元组
+字典
+>>> adic.keys()
+dict_keys(['a', 'b', 'c'])
+>>> adic.values()
+dict_values([1, 2, 3])
+>>> adic.items()
+dict_items([('a', 1), ('b', 2), ('c', 3)])
+>>>
+
+集合
+时间元组
