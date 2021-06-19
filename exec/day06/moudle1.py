34afa692669db28eaa7f3b479501a8a80f4c9229
yingxingtianxia <903511044@qq.com>
2021-06-19 19:49:44 +0800

    2021-06-19 19:49:43

--- exec/day06/moudle1.py
+++ exec/day06/moudle1.py
@@ -1,104 +1,104 @@
-#!/usr/bin/env python
-import shutil
-shutil.copyfileobj()
-
->>> f1 = open('/etc/hosts', 'rb')
->>> f2 = open('/tmp/hosts', 'a+b')
->>> shutil.copyfileobj(f1,f2,4096)
->>> f1.close()
->>> f2.close()
-
-
-shutil.copy()
-shutil.copy2()
-shutil.move()
->>> shutil.copy('/etc/hosts','/tmp/hosts1')
-'/tmp/hosts1'
->>> shutil.copy2('/etc/hosts','/tmp/hosts2')
-'/tmp/hosts2'
->>> shutil.copy2('/etc/hosts','/var/tmp/')
-'/var/tmp/hosts'
->>> shutil.move('/var/tmp/hosts','/tmp/zhujilieibao')
-'/tmp/zhujilieibao'
-
-
-
-
->>> shutil.copytree('/home/mark/cfg/','/tmp/immediate')
-'/tmp/immediate'
->>> shutil.rmtree('/tmp/immediate')
-
-
-
-
-mark@mark-PC:~$ ll /tmp/hosts
--rw-r--r-- 1 mark mark 232 6月  18 21:51 /tmp/hosts
-mark@mark-PC:~$ ll /bin/ls
--rwxr-xr-x 1 root root 138856 9月  12  2020 /bin/ls
->>> shutil.copymode('/bin/ls','/tmp/hosts')
-mark@mark-PC:~$ ll /tmp/hosts
--rwxr-xr-x 1 mark mark 232 6月  18 21:51 /tmp/hosts
-mark@mark-PC:~$
-
-mark@mark-PC:~$ stat /tmp/hosts
-  文件：/tmp/hosts
-  大小：232             块：8          IO 块：4096   普通文件
-设备：10306h/66310d     Inode：4456507     硬链接：1
-权限：(0755/-rwxr-xr-x)  Uid：( 1000/    mark)   Gid：( 1000/    mark)
-最近访问：2021-06-18 21:51:21.952014745 +0800
-最近更改：2021-06-18 21:51:11.676014407 +0800
-最近改动：2021-06-18 22:14:04.560059601 +0800
-创建时间：-
-shutil.copystat('/bin/ls','/tmp/hosts')
-mark@mark-PC:~$ stat /tmp/hosts
-  文件：/tmp/hosts
-  大小：232             块：8          IO 块：4096   普通文件
-设备：10306h/66310d     Inode：4456507     硬链接：1
-权限：(0755/-rwxr-xr-x)  Uid：( 1000/    mark)   Gid：( 1000/    mark)
-最近访问：2021-06-18 19:45:04.667961442 +0800
-最近更改：2020-09-12 20:51:04.662037290 +0800
-最近改动：2021-06-18 22:15:41.140062780 +0800
-创建时间：-
-
-mark@mark-PC:~$ ll /tmp/hosts
--rwxr-xr-x 1 mark mark 232 9月  12  2020 /tmp/hosts
->>> shutil.chown('/tmp/hosts','root','root')
-mark@mark-PC:~$ ll /tmp/hosts
--rwxr-xr-x 1 root root 232 9月  12  2020 /tmp/hosts
-
-
-
-
-
-################################3
-import subprocess
-subprocess.run()
->>> subprocess.run(['ls', '-l', 'a.sh'])
->>> subprocess.run('ls -l a.sh',shell=True)
->>> s = subprocess.run('ls -l a.sh',shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
->>> s.args
-'ls -l a.sh'
->>> s.returncode
-0
->>> s.stdout
-b'-rw-r--r-- 1 mark mark 20 6\xe6\x9c\x88  17 22:21 a.sh\n'
->>> s.stderr
-b''
->>>
-
-
-
-
->>> subprocess.getoutput('ls')
-'模板\na.sh\ncfg\ndata\nDesktop\nDocuments\nDownloads\nMusic\nPictures\nsensors\nSunloginRemote\nVideos'
->>> s = subprocess.getoutput('ls')
->>> s
-'模板\na.sh\ncfg\ndata\nDesktop\nDocuments\nDownloads\nMusic\nPictures\nsensors\nSunloginRemote\nVideos'
->>> subprocess.getstatusoutput('ls')
-(0, '模板\na.sh\ncfg\ndata\nDesktop\nDocuments\nDownloads\nMusic\nPictures\nsensors\nSunloginRemote\nVideos')
->>> s = subprocess.getstatusoutput('ls')
->>> s[0]
-0
->>> s[1]
-'模板\na.sh\ncfg\ndata\nDesktop\nDocuments\nDownloads\nMusic\nPictures\nsensors\nSunloginRemote\nVideos'
->>>
+# #!/usr/bin/env python
+# import shutil
+# shutil.copyfileobj()
+#
+# >>> f1 = open('/etc/hosts', 'rb')
+# >>> f2 = open('/tmp/hosts', 'a+b')
+# >>> shutil.copyfileobj(f1,f2,4096)
+# >>> f1.close()
+# >>> f2.close()
+#
+#
+# shutil.copy()
+# shutil.copy2()
+# shutil.move()
+# >>> shutil.copy('/etc/hosts','/tmp/hosts1')
+# '/tmp/hosts1'
+# >>> shutil.copy2('/etc/hosts','/tmp/hosts2')
+# '/tmp/hosts2'
+# >>> shutil.copy2('/etc/hosts','/var/tmp/')
+# '/var/tmp/hosts'
+# >>> shutil.move('/var/tmp/hosts','/tmp/zhujilieibao')
+# '/tmp/zhujilieibao'
+#
+#
+#
+#
+# >>> shutil.copytree('/home/mark/cfg/','/tmp/immediate')
+# '/tmp/immediate'
+# >>> shutil.rmtree('/tmp/immediate')
+#
+#
+#
+#
+# mark@mark-PC:~$ ll /tmp/hosts
+# -rw-r--r-- 1 mark mark 232 6月  18 21:51 /tmp/hosts
+# mark@mark-PC:~$ ll /bin/ls
+# -rwxr-xr-x 1 root root 138856 9月  12  2020 /bin/ls
+# >>> shutil.copymode('/bin/ls','/tmp/hosts')
+# mark@mark-PC:~$ ll /tmp/hosts
+# -rwxr-xr-x 1 mark mark 232 6月  18 21:51 /tmp/hosts
+# mark@mark-PC:~$
+#
+# mark@mark-PC:~$ stat /tmp/hosts
+#   文件：/tmp/hosts
+#   大小：232             块：8          IO 块：4096   普通文件
+# 设备：10306h/66310d     Inode：4456507     硬链接：1
+# 权限：(0755/-rwxr-xr-x)  Uid：( 1000/    mark)   Gid：( 1000/    mark)
+# 最近访问：2021-06-18 21:51:21.952014745 +0800
+# 最近更改：2021-06-18 21:51:11.676014407 +0800
+# 最近改动：2021-06-18 22:14:04.560059601 +0800
+# 创建时间：-
+# shutil.copystat('/bin/ls','/tmp/hosts')
+# mark@mark-PC:~$ stat /tmp/hosts
+#   文件：/tmp/hosts
+#   大小：232             块：8          IO 块：4096   普通文件
+# 设备：10306h/66310d     Inode：4456507     硬链接：1
+# 权限：(0755/-rwxr-xr-x)  Uid：( 1000/    mark)   Gid：( 1000/    mark)
+# 最近访问：2021-06-18 19:45:04.667961442 +0800
+# 最近更改：2020-09-12 20:51:04.662037290 +0800
+# 最近改动：2021-06-18 22:15:41.140062780 +0800
+# 创建时间：-
+#
+# mark@mark-PC:~$ ll /tmp/hosts
+# -rwxr-xr-x 1 mark mark 232 9月  12  2020 /tmp/hosts
+# >>> shutil.chown('/tmp/hosts','root','root')
+# mark@mark-PC:~$ ll /tmp/hosts
+# -rwxr-xr-x 1 root root 232 9月  12  2020 /tmp/hosts
+#
+#
+#
+#
+#
+# ################################3
+# import subprocess
+# subprocess.run()
+# >>> subprocess.run(['ls', '-l', 'a.sh'])
+# >>> subprocess.run('ls -l a.sh',shell=True)
+# >>> s = subprocess.run('ls -l a.sh',shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+# >>> s.args
+# 'ls -l a.sh'
+# >>> s.returncode
+# 0
+# >>> s.stdout
+# b'-rw-r--r-- 1 mark mark 20 6\xe6\x9c\x88  17 22:21 a.sh\n'
+# >>> s.stderr
+# b''
+# >>>
+#
+#
+#
+#
+# >>> subprocess.getoutput('ls')
+# '模板\na.sh\ncfg\ndata\nDesktop\nDocuments\nDownloads\nMusic\nPictures\nsensors\nSunloginRemote\nVideos'
+# >>> s = subprocess.getoutput('ls')
+# >>> s
+# '模板\na.sh\ncfg\ndata\nDesktop\nDocuments\nDownloads\nMusic\nPictures\nsensors\nSunloginRemote\nVideos'
+# >>> subprocess.getstatusoutput('ls')
+# (0, '模板\na.sh\ncfg\ndata\nDesktop\nDocuments\nDownloads\nMusic\nPictures\nsensors\nSunloginRemote\nVideos')
+# >>> s = subprocess.getstatusoutput('ls')
+# >>> s[0]
+# 0
+# >>> s[1]
+# '模板\na.sh\ncfg\ndata\nDesktop\nDocuments\nDownloads\nMusic\nPictures\nsensors\nSunloginRemote\nVideos'
+# >>>
---
+++ exec/day06/pyping.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+'''
+调用 ping 命令
+1. 编写 ping 函数
+2. 用于测试远程主机的联通性
+3. ping 通显示：x.x.x.x:up
+4. ping 不通显示：x.x.x.x:down
+'''
+import sys
+from subprocess import run,getstatusoutput
+
+def ping(host):
+   # s = run('ping -c 2 %s &> /dev/null ' % host,shell=True)
+    s = getstatusoutput('ping -c 2 %s' % host)
+    # if s.returncode == 0:
+    if s[0] == 0:
+        return '%s:up' % host
+    else:
+        return '%s:down' % host
+
+if __name__ == '__main__':
+    host = sys.argv[1]
+    s = ping(host)
+    print(s)
