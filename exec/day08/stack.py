6e05513038d215c842eed16ebda243ce7efcc4e5
yingxingtianxia <903511044@qq.com>
2021-06-21 22:09:17 +0800

    2021-06-21 22:09:16

---
+++ exec/day08/stack.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+'''
+用列表构建栈结构
+1. 栈是一个后进先出的结构
+2. 编写一个程序，用列表实现栈结构
+3. 需要支持压栈、出栈、查询功能
+'''
+
+stack = []
+
+
+def push_it():
+    val = input('请输入需要压栈的元素：')
+    if val:
+        stack.append(val)
+
+
+def pop_it():
+    if not stack:
+        print('当前栈为空，不可执行出栈操作')
+    else:
+        stack.pop()
+
+
+def view_it():
+    if not stack:
+        print('当前栈为空')
+    else:
+        print(stack)
+
+
+if __name__ == '__main__':
+
+    prompt = '''####命令菜单####
+0-->压栈
+1-->出栈
+2-->查询
+3-->退出
+请做出你的选择：'''
+    while True:
+        choice = input(prompt)
+        if choice not in '0123':
+            print('输入非法选项，请重新输入')
+            continue
+        choice = int(choice)
+        if choice == 3:
+            print('\n再见')
+            break
+        if choice == 0:
+            push_it()
+        elif choice == 1:
+            pop_it()
+        elif choice == 2:
+            view_it()
