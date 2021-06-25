0a7406b176d4871ca8068e5a220b305f8ecbdeb0
yingxingtianxia <903511044@qq.com>
2021-06-25 22:30:48 +0800

    2021-06-25 22:30:48

---
+++ exec/day12/account.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+'''
+账本程序
+1. 假设在记账时，有一万元钱
+2. 无论是开销还是收入都要进行记账
+3. 记账内容包括时间、金额和说明等
+4. 记账数据要求永久存储
+'''
+
+
+import pickle
+from time import strftime
+
+
+def save():
+    pass
+
+def cost():
+    pass
+
+def query():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
