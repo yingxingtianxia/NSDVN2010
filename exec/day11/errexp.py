6845a2dfd76d48fe21d1de54e7ecc504b6bb4e8d
yingxingtianxia <903511044@qq.com>
2021-06-25 20:10:47 +0800

    2021-06-25 20:10:47

---
+++ exec/day11/errexp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+'''
+简化除法判断
+1. 提示用户输入一个数字作为除法
+2. 如果用户按下 Ctrl + C 或 Ctrl + D 则退出程序
+3. 如果用户输入非数字字符，提示用户应该输入数字
+4. 如果用户输入 0，提示用户 0 不能作为除法
+'''
+while True:
+    try:
+        num = input('请输入数字：')
+        inum = int(num)
+        res = 100 / inum
+    except ValueError as e:
+        print(e,'必须输入数字')
+        continue
+    except (KeyboardInterrupt,IOError):
+        print('\nBye')
+        break
+    except ZeroDivisionError:
+        print('0不能作为被除数')
+        continue
+    else:
+        print(res)
+    finally:
+        print('Done')
