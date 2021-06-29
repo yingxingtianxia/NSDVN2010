3d7e23baf4b4b9af49319d07c7b891eb625fbf9b
yingxingtianxia <903511044@qq.com>
2021-06-29 19:36:41 +0800

    2021-06-29 19:36:40

--- exec/day14/gen_file.py
+++ exec/day14/gen_file.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python
+'''
+    文件生成器
+    - 通过生成器完成以下功能：
+      - [x] 使用函数实现生成器
+      - [x] 函数接受一个文件作为参数
+      - [x] 生成器函数每次返回文件的 10 行数据
+'''
+def gen_block(fname):
+    lines = []
+    # counter = 0
+    with open(fname,'r') as fobj:
+        for line in fobj:
+            lines.append(line)
+            # counter += 1
+            if len(lines) == 10:
+                yield lines
+                # counter = 0
+                lines = []
+        if lines:
+            yield lines
+
+if __name__ == '__main__':
+    fname = 'passwd'
+    data = gen_block(fname)
+    for i in data:
+        print(i)
