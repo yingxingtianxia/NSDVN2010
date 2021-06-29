4aaaa6dc3cffa1314099347db9e8c375bd377d5b
yingxingtianxia <903511044@qq.com>
2021-06-29 22:12:07 +0800

    2021-06-29 22:12:06

--- exec/day14/backupfile.py
+++ exec/day14/backupfile.py
@@ -12,11 +12,12 @@ import hashlib
 import pickle
 import os
 
+
 def check_md5(filename):
     m = hashlib.md5()
     with open(filename, 'rb') as fobj:
         while True:
-            data = fobj.read(4096)          #每次读取4K的数据
+            data = fobj.read(4096)  # 每次读取4K的数据
             if not data:
                 break
             m.update(data)
@@ -24,59 +25,58 @@ def check_md5(filename):
     return m.hexdigest()
 
 
-def full_back(src,dst,md5file):
-    #组装完整备份文件名（绝对路径）
+def full_back(src, dst, md5file):
+    # 组装完整备份文件名（绝对路径）
     fname = os.path.basename(src)
     date = time.strftime('%Y_%m_%d')
-    fname = '%s_full_%s.tar.gz' % (fname,date)   # c_full_xxxx_xx_xx.tar.gz
-    fname = os.path.join(dst,fname)         #/backup/c_full_xxxx_xx_xx.tar.gz
+    fname = '%s_full_%s.tar.gz' % (fname, date)  # c_full_xxxx_xx_xx.tar.gz
+    fname = os.path.join(dst, fname)  # /backup/c_full_xxxx_xx_xx.tar.gz
 
-    #执行完整备份操作
-    tar = tarfile.open(fname,'w:gz')
+    # 执行完整备份操作
+    tar = tarfile.open(fname, 'w:gz')
     tar.add(src)
     tar.close()
 
-    #md5校验变量{filename: md5value}
+    # md5校验变量{filename: md5value}
     md5_values = {}
-    for path,floders,files in os.walk(src):
+    for path, floders, files in os.walk(src):
         for file in files:
-            key = os.path.join(path,file)
+            key = os.path.join(path, file)
             md5_values[key] = check_md5(key)
 
-    #将源目录下所有文件的md5校验值字典写入本地文件
-    with open(md5file,'wb') as fobj:
-        pickle.dump(md5_values,fobj)
+    # 将源目录下所有文件的md5校验值字典写入本地文件
+    with open(md5file, 'wb') as fobj:
+        pickle.dump(md5_values, fobj)
 
 
-def incr_back(src,dst,md5file):
-    #组装增量备份文件名（绝对路径）
+def incr_back(src, dst, md5file):
+    # 组装增量备份文件名（绝对路径）
     fname = os.path.basename(src)
     date = time.strftime('%Y_%m_%d')
-    fname = '%s_incr_%s.tar.gz' % (fname,date)  # c_incr_xxxx_xx_xx.tar.gz
-    fname = os.path.join(dst,fname)     #/backup/c_incr_xxxx_xx_xx.tar.gz
+    fname = '%s_incr_%s.tar.gz' % (fname, date)  # c_incr_xxxx_xx_xx.tar.gz
+    fname = os.path.join(dst, fname)  # /backup/c_incr_xxxx_xx_xx.tar.gz
 
-    #读取上次一备份md5file的内容
-    with open(md5file,'rb') as fobj:
-        old_md5_values = pickle.load(fobj)      #获取前一天的所有文件md5值，格式是字典
+    # 读取上次一备份md5file的内容
+    with open(md5file, 'rb') as fobj:
+        old_md5_values = pickle.load(fobj)  # 获取前一天的所有文件md5值，格式是字典
 
-    #获取当前时间源目录下所有文件的md5值
+    # 获取当前时间源目录下所有文件的md5值
     md5_dic = {}
-    for path,floders,files in os.walk(src):
+    for path, floders, files in os.walk(src):
         for file in files:
-            key = os.path.join(path,file)
+            key = os.path.join(path, file)
             md5_dic[key] = check_md5(key)
 
-    #查找发生变化（md5值不一致）的文件，将其放入增量备份的tar包
+    # 查找发生变化（md5值不一致）的文件，将其放入增量备份的tar包
     tar = tarfile.open(fname, 'w:gz')
     for key in md5_dic:
         if md5_dic[key] != old_md5_values[key]:
             tar.add(key)
     tar.close()
 
-    #更新本日md5file的值，覆盖旧的md5file
+    # 更新本日md5file的值，覆盖旧的md5file
     with open(md5file, 'wb') as fobj:
         pickle.dump(md5_dic)
-
 
 
 if __name__ == '__main__':
@@ -84,6 +84,6 @@ if __name__ == '__main__':
     dst = '/tmp/backup'
     md5file = '/tmp/backup/md5file'
     if time.localtime()[6] == 0:
-        full_back()
+        full_back(src,dst,md5file)
     else:
-        incr_back()
+        incr_back(src,dst,md5file)
