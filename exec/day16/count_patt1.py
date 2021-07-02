ee60717f42626c780ab3d93a0e948b4733406c64
yingxingtianxia <903511044@qq.com>
2021-07-02 21:59:53 +0800

    2021-07-02 21:59:53

---
+++ exec/day16/count_patt1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+'''
+使用正则表达式，匹配apache日志文件，统计每个IP地址出现的次数
+'''
+import re
+from pprint import pprint
+
+def count_patt(fname,patt):
+    patt_dic = {}
+    spatt = re.compile(patt)
+    with open(fname,'r') as fobj:
+        for line in fobj:
+            m = spatt.match(line)
+            if m:
+                key = m.group()
+                if key not in patt_dic:
+                    patt_dic[key] = 1
+                else:
+                    patt_dic[key] += 1
+    return patt_dic
+
+if __name__ == '__main__':
+    fname = 'apache.log'
+    ip = '(\d+\.){3}\d+'
+    res = count_patt(fname,ip)
+    pprint(res)
--- exec/day16/per.py
+++ exec/day16/per.py
@@ -12,6 +12,12 @@ class Person:
         return '我是__repr__方法'
     def __str__(self):
         return '我是__str__方法'
+    def __call__(self, name,age,gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    def __del__(self):
+        print('%s运动完了盲肠炎，加上手术感染，挂球了。。。' % self.name)
 
     #实例方法
     def eat(self,time,food):
@@ -20,11 +26,21 @@ class Person:
         return '%s今天吃顶了，喝了%s促进消化' % (self.name,drink)
     def run(self,distance):
         return '%s还是撑得慌，去跑了%s公里' % (self.name,distance)
+    def operation(self,locale,oper):
+        self.__call__(self.name,self.age,'女')
+        return '%s去%s做了个%s手术' % (self.name,locale,oper)
 
 
 if __name__ == '__main__':
     p1 = Person('王宝宝',22,'男')
     print(p1)
+
+    print('我是%s,%s,今年%s,' % (p1.name,p1.gender,p1.age))
+    print(p1.operation('泰国','变性'))
+    print('我是%s,%s,今年%s,' % (p1.name, p1.gender, p1.age))
+
     print(p1.eat('早上','豆汁'))
     print(p1.eat('中午','卤煮'))
     print(p1.eat('晚上','炖吊子'))
+    print(p1.drink('酸奶'))
+    print(p1.run(8))
